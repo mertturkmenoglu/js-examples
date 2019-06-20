616e06f2a106e39d6396d68500f50671f8152319
Mert <mertturkmenoglu99@gmail.com>
2019-06-20 21:14:36 +0300

    Add function example

---
+++ E004_Functions/script.js
@@ -0,0 +1,14 @@
+/**
+ * Function example
+ */
+
+function printMessage(message) {
+    console.log(message);
+}
+
+function sum(a, b) {
+    return a + b;
+}
+
+printMessage("Oh, Hi Mark!");
+console.log(sum(3, 4));
